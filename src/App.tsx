4059f9acc0a078b7dfbda68f82c80f3bd164c986
Prabin Subedi <42871240+prabincankod@users.noreply.github.com>
2025-08-19 00:59:49 +0545

    ⚡️ init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+      hello krsn
+
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/manifest.json',
+          dest: '.',
+        }
+      ],
+    }),
+  ],
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
+});
