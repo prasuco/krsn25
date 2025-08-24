44a85ef3a994e419021960bd8224f194fd00bc4d
Prabin Subedi <42871240+prabincankod@users.noreply.github.com>
2025-08-24 23:41:29 +0545

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 import tailwindcss from '@tailwindcss/vite';
 
