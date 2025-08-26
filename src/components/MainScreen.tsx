a5719a6dc8670d626d8204d3b246fe5c13115cc0
Prabin Subedi <42871240+prabincankod@users.noreply.github.com>
2025-08-27 02:27:40 +0545

    feat: added 25 quotes

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -1,25 +1,24 @@
+import { quotes } from "../quotes";
+
 const MainScreen = () => {
+  const quote = quotes[Math.floor(Math.random() * quotes.length)];
+
   return (
     <main className="min-h-screen flex items-center justify-center bg-amber-50/50 px-8 py-12">
       <article className="text-center max-w-4xl mx-auto space-y-8">
         <header className="space-y-4">
           <h1 className="text-5xl md:text-6xl font-bold text-primary-500 mb-6">
-            Karma Yoga
+            {quote.chapter_name}
           </h1>
         </header>
 
         <section className="space-y-8">
           <div className="text-3xl md:text-4xl font-medium text-background leading-relaxed">
-            कर्मण्येवाधिकारस्ते मा फलेषु कदाचन।
-            <br />
-            मा कर्मफलहेतुर्भूर्मा ते सङ्गोऽस्त्वकर्मणि॥
+            {quote.sanskrit}
           </div>
 
           <div className="text-xl md:text-2xl text-muted-foreground leading-relaxed max-w-3xl mx-auto">
-            You have a right to perform your prescribed duty, but you are not
-            entitled to the fruits of action. Never consider yourself the cause
-            of the results of your activities, and never be attached to not
-            doing your duty."
+            {quote.english}
           </div>
         </section>
       </article>
